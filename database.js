d45a77c635ed3226bd1725fe12919bf97bd2544b
Susetyo Pambudi <susetyamulia@gmail.com>
2022-11-12 15:05:35 +0700

    test connection db

--- database.js
+++ database.js
@@ -4,11 +4,13 @@ const mysql = require('mysql');
 // buat konfigurasi koneksi
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '5U53ty0mul14p4mbud1!',
-    database: 'majoin-wms',
+    user: 'majoinco_admin',
+    password: 'plokijuh1234!',
+    database: 'majoinco_wms',
     multipleStatements: true
 });
+
+//majoinco_wms | majoinco_admin | majoincoadmin12345
 
 // koneksi database
 connection.connect((err) => {
